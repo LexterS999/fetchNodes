ca0bfe99dbd977226545f9be6706e709978825ba
LexterS999 <kaliostro18@gmail.com>
2025-01-20 22:24:42 +0500

    Update main.py

--- main.py
+++ main.py
@@ -174,7 +174,7 @@ def main():
         lines = f.readlines()
 
     num_lines = len(lines)
-    max_lines_per_file = 6000
+    max_lines_per_file = 2000
     num_files = (num_lines + max_lines_per_file - 1) // max_lines_per_file
 
     for i in range(num_files):
